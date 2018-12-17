63149b8e7763b0bbf0744d5be37a1167215be123
AidoWedo <40786162+AidoWedo@users.noreply.github.com>
2018-12-17 11:47:24 +0000

    Update base64decode.py

--- base64decode.py
+++ base64decode.py
@@ -1,5 +1,5 @@
 # Simple Base 64 decode
 import base64
 #replace xxxxxxxxxxx with something that is base 64
-b64coded_string = '''xxxxxxxxxxx''''
+b64coded_string = '''xxxxxxxxxxx'''
 base64.b64decode(b64coded_string)
